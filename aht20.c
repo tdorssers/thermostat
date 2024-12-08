1fe95ff3109d74151deed26a2a792873b0bba93c
Tim Dorssers <39435669+tdorssers@users.noreply.github.com>
2024-12-08 16:13:54 +0100

    Add files via upload

--- aht20.c
+++ aht20.c
@@ -5,21 +5,11 @@
  *  Author: Tim Dorssers
  */ 
 
-#define F_CPU 8000000
+#define F_CPU 1000000
 
 #include <util/delay.h>
 #include "aht20.h"
 #include "i2c.h"
-
-void aht20_init(void) {
-	/* Set initialization register */
-	i2c_start(AHTXX_ADDRESS << 1 | I2C_WRITE);
-	i2c_write(AHT2X_INIT_REG);
-	i2c_write(0x08);
-	i2c_write(0x00);
-	i2c_stop();
-	_delay_ms(10);
-}
 
 uint8_t aht20_get(uint16_t *humid, int16_t *temperature) {
 	uint8_t data[7];
@@ -29,13 +19,21 @@ uint8_t aht20_get(uint16_t *humid, int16_t *temperature) {
 		return 1;
 	}
 	i2c_write(AHTXX_STATUS_REG);
-	i2c_stop();
 	/* check calibration bit */
-	i2c_start(AHTXX_ADDRESS << 1 | I2C_READ);
+	i2c_rep_start(AHTXX_ADDRESS << 1 | I2C_READ);
 	uint8_t status = i2c_readNak();
 	i2c_stop();
-	if (!(status & 0x08)) aht20_init();
+	if (!(status & 0x08)) {
+		/* Set initialization register */
+		i2c_start(AHTXX_ADDRESS << 1 | I2C_WRITE);
+		i2c_write(AHT2X_INIT_REG);
+		i2c_write(0x08);
+		i2c_write(0x00);
+		i2c_stop();
+		_delay_ms(10);
+	}
 	/* send measurement command */
+	i2c_start(AHTXX_ADDRESS << 1 | I2C_WRITE);
 	i2c_write(AHTXX_START_MEASUREMENT_REG);
 	i2c_write(0x33);
 	i2c_write(0x00);
@@ -50,7 +48,9 @@ uint8_t aht20_get(uint16_t *humid, int16_t *temperature) {
 	}
 	data[6] = i2c_readNak();
 	i2c_stop();
-	i2c_init();  // Reinitialize TWI in case of error condition
+	/* Reinitialize TWI in case of error condition */
+	i2c_init();
+	/* Check CRC */
 	if (crc != data[6]) return 2;
 	/*  Calculate the temperature and humidity value */
 	uint32_t hum = ((uint32_t)data[1] << 12) | ((uint16_t)data[2] << 4) | (data[3] >> 4);
